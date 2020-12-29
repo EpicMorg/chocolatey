ee0d416ddc5ca811c432a64e8856becb716ca626
stam <stam@epicm.org>
2020-12-30 02:41:22 +0300

    0.0.18

---
+++ chocolatey/airtest-ide/0.0.18/README.md
@@ -0,0 +1,19 @@
+# V0.0.18
+## Release Date: 2018-05-22
+
+### New Features
+- Add poco support for iOS native apps
+### Bug Fixes
+- Optimize Home button style for iOS device screen
+- Fix the abnormal occupancy problem of adb.exe file descriptor
+- Fix a crash issue that happens after switching record/edit mode of selenium plugin
+- Fix the problem that a phone with STF-installed cannot be initialized correctly
+- Fix a crash issue that happens when debugging without a phone connected
+- Fix the issue that 'sleep' statement without any arguments causes the report generation failure
+- Fix the issue that ‘assert’ statement cannot write log into log.file when an error occurs
+- Fix the problem that users cannot use exec_script with py3
+- Fix the problem that screenshots cannot be saved in directories with some Chinese characters
+
+### Download
+- Windows: [AirtestIDE_2018-05-22_18.zip](http://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip)
+- Mac: [AirtestIDE_Mac_2018-05-22_18.dmg](http://top.gdl.netease.com/AirtestIDE_Mac_2018-05-22_18.dmg)
---
+++ chocolatey/airtest-ide/0.0.18/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+[[AutomaticPackageNotesInstaller]]
+$packageName= 'airtest-ide'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+#$fileLocation = Join-Path $toolsDir 'AirtestIDE_2018-05-22_18.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'http://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip' # download url, HTTPS preferred
+  url64bit      = 'http://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  destination   = $toolsDir
+
+  checksum      = '09459651f7da58cd4e7d902ee79757f6506d70390aac0004158642dd3909499a'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = '09459651f7da58cd4e7d902ee79757f6506d70390aac0004158642dd3909499a'
+  checksumType64= 'sha256'
+}
+
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+Install-ChocolateyZipPackage @packageArgs
+
+## Unzips a file to the specified location - auto overwrites existing content
+## - https://chocolatey.org/docs/helpers-get-chocolatey-unzip
+#Get-ChocolateyUnzip @packageArgs

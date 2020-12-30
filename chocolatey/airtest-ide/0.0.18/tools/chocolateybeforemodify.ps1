3af1c174f3d5c6900c2c719a819fbac50ad9b42b
stam <stam@epicm.org>
2020-12-30 15:59:40 +0300

    0.0.18.20180522 update

--- chocolatey/airtest-ide/0.0.18/tools/chocolateybeforemodify.ps1
+++ chocolatey/airtest-ide/0.0.18/tools/chocolateybeforemodify.ps1
@@ -22,86 +22,78 @@ $ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Progra
 $DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 
 $airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
-$airtestBin = Join-Path $airtestFolder 'AirtestIDE.exe'
-$adbAirtestBin Join-Path $airtestFolder 'airtest\core\android\static\adb\windows\adb.exe'
-$chromedriverAirtestBin Join-Path $airtestFolder 'chromedriver.exe'
-$qtWebEngineProcessAirtestBin Join-Path $airtestFolder 'QtWebEngineProcess.exe'
+$airtestBin = Join-Path $airtestFolder 'AirtestIDE.exe' 
+$adbAirtestFolder = Join-Path -Path  '$airtestFolder' -ChildPath 'airtest\core\android\static\adb\windows'
+$adbAirtestBin = Join-Path '$adbAirtestFolder' 'adb.exe'
+$chromedriverAirtestBin = Join-Path $airtestFolder 'chromedriver.exe'
+$qtWebEngineProcessAirtestBin = Join-Path $airtestFolder 'QtWebEngineProcess.exe'
 
-
-################################################################################
-# Trying to terminate process
-################################################################################
-try {
-    Get-Process | Where-Object {$_.Path -like $airtestShimm} | Stop-Process -WhatIf
-    Write-Host 'Process $airtestShimm stopped'
-}
-catch {
-    Write-Host 'Process $airtestShimm not found. Skipping.'
-}
-
-
-try {
-    Get-Process | Where-Object {$_.Path -like $airtestBin} | Stop-Process -WhatIf
-    Write-Host 'Process $airtestBin stopped'
-}
-catch {
-    Write-Host 'Process $airtestBin not found. Skipping.'
-}
-
-try {
-    Get-Process | Where-Object {$_.Path -like $adbAirtestBin} | Stop-Process -WhatIf
-    Write-Host 'Process $adbAirtestBin stopped'
-}
-catch {
-    Write-Host 'Process adbAirtestBin not found. Skipping.'
-}
-
-try {
-    Get-Process | Where-Object {$_.Path -like $chromedriverAirtestBin} | Stop-Process -WhatIf
-    Write-Host 'Process $chromedriverAirtestBin stopped'
-}
-catch {
-    Write-Host 'Process chromedriverAirtestBin not found. Skipping.'
-}
-
-try {
-    Get-Process | Where-Object {$_.Path -like $qtWebEngineProcessAirtestBin} | Stop-Process -WhatIf
-    Write-Host 'Process $qtWebEngineProcessAirtestBin stopped'
-}
-catch {
-    Write-Host 'Process qtWebEngineProcessAirtestBin not found. Skipping.'
-}
 
 ################################################################################
 # Trying to remove shortcuts
 ################################################################################
 # From Programs menu
-try {
-    $shortcutFilePathProgramsPath = Join-Path $ProgramsPath 'AirtestIDE.lnk'
-    Remove-Item -Path $shortcutFilePathProgramsPath -Force
-    Write-Host 'Shortcut $shortcutFilePathProgramsPath removed.'
-}
-catch {
-    Write-Host 'Shortcut $shortcutFilePathProgramsPath not found. Skipping.'
-}
+# try {
+#     $shortcutFilePathProgramsPath = Join-Path $ProgramsPath 'AirtestIDE.lnk'
+#     Remove-Item -Path $shortcutFilePathProgramsPath -Force
+#     Write-Host 'Shortcut AirtestIDE.lnk removed.'
+# }
+# catch {
+#     Write-Host 'Shortcut AirtestIDE.lnk not found. Skipping.'
+# }
+# 
+# # From Desktop
+# try {
+#     $shortcutFilePathDesktopPath = Join-Path $DesktopPath 'AirtestIDE.lnk'
+#     Remove-Item -Path $shortcutFilePathDesktopPath -Force
+#     Write-Host 'Shortcut AirtestIDE.lnk removed.'
+# }
+# catch {
+#     Write-Host 'Shortcut AirtestIDE.lnk not found. Skipping.'
+# }
+# 
+# ################################################################################
+# # Trying to terminate process
+# ################################################################################
+# try {
+#     Get-Process | Where-Object {$_.Path -like $airtestShimm} | Stop-Process -Force
+#     Write-Host 'Process AirtestIDE.exe (shimm) stopped'
+# }
+# catch {
+#     Write-Host 'Process AirtestIDE.exe (shimm) not found. Skipping.'
+# }
+# 
+# 
+# try {
+#     Get-Process | Where-Object {$_.Path -like $airtestBin} | Stop-Process -Force 
+#     Write-Host 'Process AirtestIDE.exe stopped'
+# }
+# catch {
+#     Write-Host 'Process AirtestIDE.exe not found. Skipping.'
+# }
+# 
+# try {
+#     Start-Process -FilePath $adbAirtestBin -WorkingDirectory $adbAirtestFolder  -ArgumentList "kill-sever"
+#     Stop-Process -Force -Name "adb"
+#     Write-Host 'Process adb.exe stopped'
+# }
+# catch {
+#     Write-Host 'Process adb.exe not found. Skipping.'
+# }
+# 
+# try {
+#     Get-Process | Where-Object {$_.Path -like $chromedriverAirtestBin} | Stop-Process -Force
+#     Write-Host 'Process chromedriver.exe stopped'
+# }
+# catch {
+#     Write-Host 'Process chromedriver.exe not found. Skipping.'
+# }
+# 
+# try {
+#     Get-Process | Where-Object {$_.Path -like $qtWebEngineProcessAirtestBin} | Stop-Process -Force
+#     Write-Host 'Process QtWebEngineProcess.exe stopped'
+# }
+# catch {
+#     Write-Host 'Process QtWebEngineProcess.exe not found. Skipping.'
+# }
 
-# From Desktop
-try {
-    $shortcutFilePathDesktopPath = Join-Path $DesktopPath 'AirtestIDE.lnk'
-    Remove-Item -Path $shortcutFilePathDesktopPath -Force
-    Write-Host 'Shortcut $shortcutFilePathDesktopPath removed.'
-}
-catch {
-    Write-Host 'Shortcut $shortcutFilePathDesktopPath not found. Skipping.'
-}
-
-################################################################################
-# Deleting directory
-################################################################################
-try {
-    Remove-Item -Path $airtestFolder -Force
-    Write-Host 'Folder $airtestFolder removed.'
-}
-catch {
-    Write-Host 'Folder $airtestFolder not found. Skipping.'
-}

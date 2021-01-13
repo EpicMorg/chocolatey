b08d12da25ba463adb3f2d597fe1ed78f220d834
stam <stam@epicm.org>
2021-01-14 01:53:00 +0300

    edit

--- chocolatey/airtest-ide/0.0.18.20180522/tools/chocolateyinstall.ps1
+++ chocolatey/airtest-ide/0.0.18.20180522/tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $folderName
   url           = 'https://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip' # download url, HTTPS preferred
-  url64bit      = 'https://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #url64bit      = 'https://top.gdl.netease.com/AirtestIDE_2018-05-22_18.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
   #file         = $fileLocation
   #fileFullPath = $fileLocation
   #destination   = Join-Path $toolsDir $folderName
@@ -17,8 +17,8 @@ $packageArgs = @{
   checksum      = '2CF4C8DA42D3D49AB3CE2616A255F95168065F820AFF850AA77B177E1C86680F'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
-  checksum64    = '2CF4C8DA42D3D49AB3CE2616A255F95168065F820AFF850AA77B177E1C86680F'
-  checksumType64= 'sha256'
+  #checksum64    = '2CF4C8DA42D3D49AB3CE2616A255F95168065F820AFF850AA77B177E1C86680F'
+  #checksumType64= 'sha256'
 }
 
 $testProcessAdminRights = Test-ProcessAdminRights

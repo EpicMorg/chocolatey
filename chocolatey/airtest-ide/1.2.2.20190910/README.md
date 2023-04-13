01d813b17690dd801980fe2bd1cdaee38b042416
STAM <stam@epicm.org>
2023-04-14 00:59:05 +0300

    airtest-ide.1.2.2.20190910

---
+++ chocolatey/airtest-ide/1.2.2.20190910/README.md
@@ -0,0 +1,21 @@
+# V1.2.2.20190910
+## Release Date: 2019-09-10
+
+## New Features
+- Airtest version updated to 1.1.0
+- During the running of the script, the saved screenshots are compressed, greatly reducing the report file size.
+- Report style is fully iterated into a new version.
+- Fixed some issues when exporting reports when the script was a .py file.
+- Fixed an issue where iOS screen rotation direction was incorrectly determined.
+
+## Bug Fixes
+- Completely modified AirtestIDE login process.
+- For some users who are unable to open AirtestIDE, or the script editing window can not be edited, we provide a 'CompatibilityMode.bat' startup script, double-click this .bat script to start AirtestIDE.
+- Fully supplemented with Chinese translation of some English interfaces.
+- Fixed some bugs that could cause a crash.
+
+## Download
+- Windows64: [AirtestIDE_2019-09-10_py3_win64.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_win64.zip)
+- Windows32: [AirtestIDE_2019-09-10_py3_win32.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_win32.zip)
+- Mac: [AirtestIDE_2019-09-10_py3_Mac10-12.dmg](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_Mac10-12.dmg)
+- Ubuntu: [AirtestIDE_2019-09-10_py3_ubuntu16-04.deb](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_ubuntu16-04.deb)
---
+++ chocolatey/airtest-ide/1.2.2.20190910/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_win32.zip' # download url, HTTPS preferred
+  url64bit      = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2019-09-10_py3_win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '7E1AFE72648CBCA6AD7DCB55D0F83D73A8778FC60ACEAD2DABDB9F5A85674640'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '856B6EE397769449543472D94773E1125DBFF5533A2F9F00A971FAD83663DD3E'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

c2c7dfc129a65074e44288f88a735d7e7f88c907
STAM <stam@epicm.org>
2023-04-14 00:49:52 +0300

    airtest-ide 1.2.1.20190416

---
+++ chocolatey/airtest-ide/1.2.1.20190416/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://top.gdl.netease.com/AirtestIDE_2019-04-16_py3_win32.zip' # download url, HTTPS preferred
+  url64bit      = 'https://top.gdl.netease.com/AirtestIDE_2019-04-16_py3_win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '9645E752D695397AA148FDC34058100C967531B4E0999C1A059A0CE70880B62D'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '1F40CC8E7A7678D5B8A8659662DC928FBE42781F6F465A1EDF50C91921DB3388'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

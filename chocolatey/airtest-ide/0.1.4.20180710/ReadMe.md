e909c44728086c078af7974785da07ff8c5aa837
STAM <stam@epicm.org>
2023-04-13 23:46:30 +0300

    airtest-ide.0.1.4.20180710

---
+++ chocolatey/airtest-ide/0.1.4.20180710/ReadMe.md
@@ -0,0 +1,21 @@
+﻿# V0.1.4.20180710
+## Release Date: 2018-07-10
+
+## New Features
+- Add image verification feature on login page
+
+## Bug Fixes
+- Fix crash issues that may happen when running scripts in some cases
+- Fixed a bug that IDE cannot start properly after opening a Chinese path script on Mac version
+- When the Android device is disconnected, the poco data thread will be automatically disconnected from the device
+- Fix the problem that the device frame cannot be adaptively sized when connecting to a remote device
+- Fix the poco ui-property with Chinese characters cannot display normally in the log window
+- Fix the crash issue when recording the poco script
+- Fix the crash issue when the poco ui has a str type attribute
+- Fix the crash issue that may happen when you click the assertion button before recording with selenium plugin
+
+## Download
+- Windows64: [AirtestIDE_2018-07-10_py3_win64.zip](https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_win64.zip)
+- Windows32: [AirtestIDE_2018-07-10_py3_win32.zip](https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_win32.zip)
+- Mac: [AirtestIDE_2018-07-10_py3_Mac10-12.dmg](https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_Mac10-12.dmg)
+- Ubuntu: [AirtestIDE_2018-07-10_py3_ubuntu16-04.deb](https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_ubuntu16-04.deb)
---
+++ chocolatey/airtest-ide/0.1.4.20180710/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_win32.zip' # download url, HTTPS preferred
+  url64bit      = 'https://top.gdl.netease.com/AirtestIDE_2018-07-10_py3_win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '11678342BF2CBAB8BE1F0505231C0EA8A58CF7057687457B78B0B15C07F830E8'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '6A6F396A10D703591D9084EC2B5563E49E47071E33B51CB460F047491695AA19'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

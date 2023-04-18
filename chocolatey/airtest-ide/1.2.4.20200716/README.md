e00ddae11b56598d934fc65e5918a294ada3373b
STAM <stam@epicm.org>
2023-04-18 22:56:52 +0300

    1.2.4.20200716

---
+++ chocolatey/airtest-ide/1.2.4.20200716/README.md
@@ -0,0 +1,25 @@
+# V1.2.4.20200716
+## Release Date: 2020-07-16
+
+## New Features
+- Airtest has been updated to version 1.1.4, and the calling method of interface swipe_along/pinch/two_finger_swipe has been modified. Xcode support has been updated to version 11.5 and iOS version has been supported to 13.5.
+- Added automatic update function of AirtestIDE.
+- Optimized the search function of the script editing window.
+- Slightly optimized log level filtering function.
+- Right-click to run the selected code function, if there is a poco statement, it will try to initialize a poco object according to the current poco mode, which is more convenient to run.
+- AirtestIDE does not provide an Ubuntu version for this update. You can write scripts on other platforms, and then deploy a Python environment to run on Ubuntu.
+
+## Bug Fixes
+- Fixed a bug where some phones might fail to tap the screen in ADB TOUCH mode.
+- Fixed a bug that might be triggered when the script is saved as.
+- Fixed the horizontal screen abnormality of some iOS models and increase the speed of starting applications.
+- After Android Poco chooses Stop, it will no longer be forced to continuously restart the poco-service process.
+- Fixed a bug where the custom launcher.py path cannot be too long.
+- Fixed an issue where the state of the auto-record button was incorrect when disconnected and connected to the phone, and a function to automatically record and insert a set_current statement when switching phones was added.
+- When using custom python.exe, the log of the print statement can be printed correctly in order.
+- Fixed the problem that setting the Chinese log path under the mac will cause an error in report generation.
+
+## Download
+- Windows64: [AirtestIDE_2020-07-16_py3_win64.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-07-16_py3_win64.zip)
+- Windows32: [AirtestIDE_2020-07-16_py3_win32.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-07-16_py3_win32.zip)
+- Mac: [AirtestIDE_2020-07-16_py3_Mac10-12.dmg](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-07-16_py3_Mac10-12.dmg)
---
+++ chocolatey/airtest-ide/1.2.4.20200716/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-07-16_py3_win32.zip' # download url, HTTPS preferred
+  url64bit      = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-07-16_py3_win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '72913C3D58C3B0C6A417BBA876E1B1944E9F98D0F636F1BA805A2DC51A90F0CB'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '3159C5DF0661FD5814E90C196CB6F8EBE0C3769B05E298D7344BBC7523EF0821'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

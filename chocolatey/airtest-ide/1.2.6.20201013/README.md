439c6756c7c82a9bf7ea9771ee8430a0847230b6
STAM <stam@epicm.org>
2023-04-18 23:22:47 +0300

    1.2.6.20201013

---
+++ chocolatey/airtest-ide/1.2.6.20201013/README.md
@@ -0,0 +1,16 @@
+# V1.2.6.20201013
+## Release Date: 2020-10-13
+
+## New Features
+- Comprehensively modify the device connection module, and optimize the window connection experience under windows. If you encounter a BUG when connecting to a mobile phone or windows program, please give us feedback.
+- Airtest supports Android11 ​​mobile phones, and has modified multiple interfaces, as well as multiple bug fixes. 
+- If you use the local Python environment, remember to use pip install -U airtest and pip install -U pocoui to update the local environment to the latest version.
+
+## Bug Fixes
+- Fixed some problems of using IDE to connect to Poco, and when Poco failed to start, it will not be forced to reinstall Poco, but try to kill other conflicting processes.
+- Fixed some other bugs.
+
+## Download
+- Windows64: [AirtestIDE-win-1.2.6.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.6.zip)
+- Windows32: [AirtestIDE-win-1.2.6.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.6.zip)
+- Mac: [AirtestIDE-mac-1.2.6.dmg](https://airtestproject.s3.netease.com/downloads/AirtestIDE/mac/AirtestIDE-mac-1.2.6.dmg)
---
+++ chocolatey/airtest-ide/1.2.6.20201013/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.6.zip' # download url, HTTPS preferred
+  url64bit      = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.6.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = 'B271593A5DF5BE0A4A5528D7A43C4C628B02F620E91E4B8BC5757CC6F0E22BAA278D0128F8E404F253A762A7D1D6F86A74856A61A7A4646DD58937D79D4403ED'
+  checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '4E6982C8E2A692BE430B7E4C4E16B25C0F09FFB2BAE001085620DD1E6B5C47A1705C4A8789F9B5EB7432284712BC413958FFCCDE7A7F3F257B58E84DE0B06975'
+  checksumType64= 'sha512'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

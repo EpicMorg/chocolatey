e2f182e373e67627f14dee816eb310c5cba3c874
STAM <stam@epicm.org>
2023-04-18 23:05:17 +0300

    1.2.15.20230404

---
+++ chocolatey/airtest-ide/1.2.15.20230404/README.md
@@ -0,0 +1,28 @@
+# V1.2.15.20230404
+## Release Date: 2023-04-04
+
+## New Features
+- Airtest is updated to 1.2.10.1, and the updates are as follows:
+- Airtest 1.2.7
+- Added device.disconnect() interface
+- Added a lot of assertion statements
+- Modified the application installation interface pm_install
+- Support Android12L
+- Airtest 1.2.8-1.2.9
+- Windows and iOS devices support recording screens using the start_recording interface
+- Improved the compatibility of windows screenshot method
+- Support Android13
+- Airtest 1.2.10.1
+- Update support for https://github.com/AirtestProject/iOS-Tagent, please update airtest and iOS-Tagent at the same time, and deploy the latest version on the phone
+- Poco updated to 1.0.89, and the updates are as follows:
+- Update PocoService.apk to support Android12+
+- Download the iOS-Tagent project: iOS-Tagent
+
+## Bug Fixes
+- 
+
+## Download
+- Windows64: [AirtestIDE-win-1.2.15.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.15.zip)
+- Windows32: [AirtestIDE-win-1.2.15.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.15.zip)
+- Mac: [AirtestIDE-mac-1.2.15.dmg](https://airtestproject.s3.netease.com/downloads/AirtestIDE/mac/AirtestIDE-mac-1.2.15.dmg)
+ 
---
+++ chocolatey/airtest-ide/1.2.15.20230404/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.15.zip' # download url, HTTPS preferred
+  url64bit      = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.15.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '23B26D570ED2A0E230C5EF1FFE41B78AE3CE83FF7022E3BBE515D84477D606A681F2721BCA2DA5918D126CB194AED85AFFCD4AF02A0D9B60EED8419EF1F750C7'
+  checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '7242659C320D8BC39E6C66EF0351B3FD0D0A20651E3B61A983CF5D1CD49BC98887B2E274C308B5F1F2D0F3F250190EEFEFEDD58386E3FDDC3E4A4FF8AE944B3F'
+  checksumType64= 'sha512'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

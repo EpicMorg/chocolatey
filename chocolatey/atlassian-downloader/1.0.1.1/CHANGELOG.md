8d196e58bd6ae8a851b2bfa8a5bef37b5dd6d939
STAM <stam@epicm.org>
2023-07-21 12:33:50 +0300

    atlassian-downloader 1.0.1.1

---
+++ chocolatey/atlassian-downloader/1.0.1.1/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Atlassian Downloader - Changelog
+
+* `1.0.1.1` - minor update: added `UserAgent` to HTTP headers and added mirrors of json files.
---
+++ chocolatey/atlassian-downloader/1.0.1.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'atlassian-downloader'
+$folderName =  Join-Path $toolsDir 'Atlassian Downloader'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://github.com/EpicMorg/atlassian-downloader/releases/download/1.0.1.1/atlassian-downloader-net6.0-win7-x86.zip' # download url, HTTPS preferred
+  url64bit      = 'https://github.com/EpicMorg/atlassian-downloader/releases/download/1.0.1.1/atlassian-downloader-net6.0-win7-x64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = '3E1FEEDE431E67483910B415947D9842C7D10314792588EBDF2382D5676C09B9'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '1C96CA66CC90DDFA8BA6DFC58707862CACAC790379DB7D089500D8BB0697F75F'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$atlDwnldrShimm = Join-Path $chocoRootBin 'atlassian-downloader.exe'
+Write-Host "atlDwnldrShimm: $atlDwnldrShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "Atlassian Downloader.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "Atlassian Downloader.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $atlDwnldrShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $atlDwnldrShimm

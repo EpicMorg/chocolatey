ee8996af4f5cee9e0b9701b5f7e30bf6c147e3c4
STAM <stam@epicm.org>
2023-10-07 02:41:44 +0300

    1.2.11.20210819

---
+++ chocolatey/airtest-ide/1.2.11.20210819/README.md
@@ -0,0 +1,23 @@
+# V1.2.11.20210819
+## Release Date: 2021-08-19
+
+## New Features
+- none
+
+## Bug Fixes
+- Airtest is upgraded to version 1.2.2
+- Improve the mstpl image recognition algorithm, reduce the false detection rate of the target picture not in the current picture, and improve the fault tolerance rate
+- When the minitouch is initialized, an additional (0, 0) accidental click is no longer added
+- Fixed the problem that the port may not be recycled correctly when disconnected
+- For more Airtest related changes, please check the document Airtest#1.2.2
+- Poco is upgraded to version 1.0.83
+- The pocoservice.apk file has been updated to solve the problem that some Android controls cannot be correctly recognized on some models
+- If you need to update, please uninstall the two pocoservice.apk in your phone first, and then start Poco's Android mode in the latest version of AirtestIDE to automatically install the latest version
+- AirtestIDE fixes several bugs
+
+## Download
+- Windows32: [AirtestIDE-win-1.2.11.zip](https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.11.zip)
+- Windows64: [AirtestIDE-win-1.2.11.zip]https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.11.zip -O AirtestIDE-win64-1.2.11.zip)
+- Mac: [AirtestIDE-mac-1.2.11.dmg](https://airtestproject.s3.netease.com/downloads/AirtestIDE/mac/AirtestIDE-mac-1.2.11.dmg)
+- Ubuntu: [AirtestIDE_2020-01-21_py3_ubuntu16-04.deb](https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-01-21_py3_ubuntu16-04.deb)
+
---
+++ chocolatey/airtest-ide/1.2.11.20210819/tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$chocoRootBin = Join-Path $($env:ChocolateyInstall) 'bin'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'airtest-ide'
+$folderName =  Join-Path $toolsDir 'AirtestIDE'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $folderName
+  url           = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win32/AirtestIDE-win-1.2.11.zip' # download url, HTTPS preferred
+  url64bit      = 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/win64/AirtestIDE-win-1.2.11.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  #file         = $fileLocation
+  #fileFullPath = $fileLocation
+  #destination   = Join-Path $toolsDir $folderName
+
+  checksum      = 'B27E87EB9BAD08EA9BC9409C7F15C53EB5951721695B039D711E717FE0851115'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  checksum64    = '1749032F4BB6D7FBB46455A12EB0CFA33CC7A34EDC7AAEC9A64EB9275864EFC9'
+  checksumType64= 'sha256'
+}
+
+$testProcessAdminRights = Test-ProcessAdminRights
+Write-Host "ProcessAdminRights:" $testProcessAdminRights
+
+################################################################################
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+################################################################################
+Install-ChocolateyZipPackage @packageArgs 
+
+################################################################################
+# Install  shortcuts
+################################################################################
+$airtestShimm = Join-Path $chocoRootBin 'AirtestIDE.exe'
+Write-Host "airtestShimm: $airtestShimm"
+
+$ProgramsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+$shortcutFilePathProgramsPath = Join-Path $ProgramsPath "AirtestIDE.lnk"
+$shortcutFilePathDesktopPath = Join-Path $DesktopPath "AirtestIDE.lnk"
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathProgramsPath -targetPath $airtestShimm
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePathDesktopPath -targetPath $airtestShimm

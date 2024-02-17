d5a81876e843d7eb7604bc8b07e17c216bfb5e2b
STAM <stam@epicm.org>
2024-02-17 15:19:32 +0300

    winbox 3.38

---
+++ chocolatey/winbox/3.38/CHANGELOG.md
@@ -0,0 +1,6 @@
+# WinBox - [Changelog](https://forum.mikrotik.com/viewtopic.php?t=195839)
+
+## What's new in v3.38:
+      * fixed connecting to wireless network from QuickSet when there was no password specified;
+      * fixed date and time value representation in the User Manager;
+      * fixed skin file reading;
---
+++ chocolatey/winbox/3.38/tools/chocolateyInstall.ps1
@@ -0,0 +1,45 @@
+﻿$packageName = 'winbox'
+$appName = 'WinBox'
+$url32 = 'https://download.mikrotik.com/winbox/3.38/winbox.exe'
+$url64 = 'https://download.mikrotik.com/winbox/3.38/winbox64.exe'
+$dir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$is64 = ((Get-ProcessorBits 64) -and !$env:chocolateyForceX86)
+$processName32 = 'winbox'
+$processName64 = 'winbox64'
+$processName = If ($is64) { $processName64 } Else { $processName32 }
+$exe = $processName + '.exe'
+$fullPath = Join-Path -Path $dir -ChildPath $exe
+$checksum32 = '761dcffa8b6e9219c4a554ea4eed7a32020756725bce9e15a23fb0677af91718'
+$checksum64 = '455fa3f05422829c0fa6b476c6e318a4344c0f0ba04990fe3b6666c8f643cf9e'
+$checksumType = 'sha256'
+
+$startFolder = $appName
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    New-Item $startMenuFolderPath -Type Directory -ErrorAction SilentlyContinue
+    If (Test-Path -Path $startMenuFolderPath -PathType Container) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $dir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name $processName32 -ErrorAction SilentlyContinue
+Stop-Process -Name $processName64 -ErrorAction SilentlyContinue
+
+# Workaround for https://github.com/chocolatey/choco/issues/952
+Remove-Item -Path $fullPath -Force -Confirm:$FALSE -ErrorAction SilentlyContinue
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fullPath -Url $url32 -Url64bit $url64 -Checksum $checksum32 -ChecksumType $checksumType -Checksum64 $checksum64 -ChecksumType64 $ChecksumType
+
+# create .gui file
+New-Item -Path $dir -Name ($exe + '.gui') -ItemType file
+
+CreateStartMenuShortcut
+

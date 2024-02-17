b8d13c87e680364e5dad1c92bbe8fb4fca4a4a19
STAM <stam@epicm.org>
2024-02-17 15:29:01 +0300

    winbox 3.40

---
+++ chocolatey/winbox/3.40/CHANGELOG.md
@@ -0,0 +1,5 @@
+# WinBox - [Changelog](https://forum.mikrotik.com/viewtopic.php?t=200431)
+
+## What's new in v3.40:
+* fit BTH QR code into default settings window;
+* fixed WinBox upgrade procedure (introduced in v3.39);
---
+++ chocolatey/winbox/3.40/tools/chocolateyInstall.ps1
@@ -0,0 +1,45 @@
+﻿$packageName = 'winbox'
+$appName = 'WinBox'
+$url32 = 'https://download.mikrotik.com/winbox/3.40/winbox.exe'
+$url64 = 'https://download.mikrotik.com/winbox/3.40/winbox64.exe'
+$dir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$is64 = ((Get-ProcessorBits 64) -and !$env:chocolateyForceX86)
+$processName32 = 'winbox'
+$processName64 = 'winbox64'
+$processName = If ($is64) { $processName64 } Else { $processName32 }
+$exe = $processName + '.exe'
+$fullPath = Join-Path -Path $dir -ChildPath $exe
+$checksum32 = '91162bdf1122a4fbdda31c31f37c92aa2d41897617eb37dbe4c7a311f96c7587'
+$checksum64 = '91f7b0456619dbfc19940a85566c949ce3718321f32709886df5125c71f0b6b7'
+$checksumType = 'sha256'
+
+$startFolder = $appName
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    New-Item $startMenuFolderPath -Type Directory -ErrorAction SilentlyContinue
+    If (Test-Path -Path $startMenuFolderPath -PathType Container) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $dir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name $processName32 -ErrorAction SilentlyContinue
+Stop-Process -Name $processName64 -ErrorAction SilentlyContinue
+
+# Workaround for https://github.com/chocolatey/choco/issues/952
+Remove-Item -Path $fullPath -Force -Confirm:$FALSE -ErrorAction SilentlyContinue
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fullPath -Url $url32 -Url64bit $url64 -Checksum $checksum32 -ChecksumType $checksumType -Checksum64 $checksum64 -ChecksumType64 $ChecksumType
+
+# create .gui file
+New-Item -Path $dir -Name ($exe + '.gui') -ItemType file
+
+CreateStartMenuShortcut
+

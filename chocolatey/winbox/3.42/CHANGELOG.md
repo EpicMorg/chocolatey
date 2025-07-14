57b24b4b64ec557258f58c158ded177bef7d22fb
STAM <stam@epicm.org>
2025-07-14 12:44:00 +0300

    winbox 3.42

---
+++ chocolatey/winbox/3.42/CHANGELOG.md
@@ -0,0 +1,8 @@
+## WinBox - [Changelog](https://forum.mikrotik.com/t/winbox-v3-42-released/183910)
+
+### What's new in v3.42:
+* added support for the Files menu in future RouterOS versions;
+* added some widget features for future RouterOS versions;
+* fixed crash when dragging the “#” column by disabling its movement;
+* fixed date fields to support year up to 2106;
+* terminal: ignore legacy shift-out (SO) character;
---
+++ chocolatey/winbox/3.42/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageName = 'winbox'
+$appName = 'WinBox'
+$url32 = 'https://download.mikrotik.com/routeros/winbox/3.42/winbox.exe'
+$url64 = 'https://download.mikrotik.com/routeros/winbox/3.42/winbox64.exe'
+$dir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$is64 = ((Get-ProcessorBits 64) -and !$env:chocolateyForceX86)
+$processName32 = 'winbox'
+$processName64 = 'winbox64'
+$processName = If ($is64) { $processName64 } Else { $processName32 }
+$exe = $processName + '.exe'
+$fullPath = Join-Path -Path $dir -ChildPath $exe
+$checksum32 = '2f06a207043014dfe28966d62ec43d35791fa4a52172c6335dbd661701b52ae9'
+$checksum64 = '47e83dfd0f9680d2e9623fee92c0acc4db40ea4272edeb53164304620305a24f'
+$checksumType = 'sha256'
+
+$startFolder = $appName
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    New-Item $startMenuFolderPath -Type Directory -ErrorAction SilentlyContinue
+    If (Test-Path -Path $startMenuFolderPath -PathType Container) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $dir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name $processName32 -ErrorAction SilentlyContinue
+Stop-Process -Name $processName64 -ErrorAction SilentlyContinue
+
+# Workaround for https://github.com/chocolatey/choco/issues/952
+Remove-Item -Path $fullPath -Force -Confirm:$FALSE -ErrorAction SilentlyContinue
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fullPath -Url $url32 -Url64bit $url64 -Checksum $checksum32 -ChecksumType $checksumType -Checksum64 $checksum64 -ChecksumType64 $ChecksumType
+
+# create .gui file
+New-Item -Path $dir -Name ($exe + '.gui') -ItemType file
+
+CreateStartMenuShortcut

137bee047cbe2d4f21c59056221df813cd6d4ecd
STAM <stam@epicm.org>
2025-11-23 18:51:55 +0300

    winbox 3.43

---
+++ chocolatey/winbox/3.43/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$packageName = 'winbox'
+$appName = 'WinBox'
+$url32 = 'https://download.mikrotik.com/routeros/winbox/3.43/winbox.exe'
+$url64 = 'https://download.mikrotik.com/routeros/winbox/3.43/winbox64.exe'
+$dir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$is64 = ((Get-ProcessorBits 64) -and !$env:chocolateyForceX86)
+$processName32 = 'winbox'
+$processName64 = 'winbox64'
+$processName = If ($is64) { $processName64 } Else { $processName32 }
+$exe = $processName + '.exe'
+$fullPath = Join-Path -Path $dir -ChildPath $exe
+$checksum32 = 'A4038E4E09A34285C8DA8D8C2930EE38EA5BEEAD2B6FFCD60130CDC8030E2E6B'
+$checksum64 = '5B41CF51FD81E8D082687F6B522159CF4ABA22E6DF8EDC486721D4E09507B6E9'
+$checksumType = 'sha256'
+
+$startFolder = $appName
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) `
+        -ChildPath $startFolder
+    New-Item $startMenuFolderPath -Type Directory -ErrorAction SilentlyContinue
+    If (Test-Path -Path $startMenuFolderPath -PathType Container) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $dir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+# SCRIPT STARTS HERE #
+
+Stop-Process -Name $processName32 -ErrorAction SilentlyContinue
+Stop-Process -Name $processName64 -ErrorAction SilentlyContinue
+
+# Workaround for https://github.com/chocolatey/choco/issues/952
+Remove-Item -Path $fullPath -Force -Confirm:$FALSE -ErrorAction SilentlyContinue
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $fullPath -Url $url32 -Url64bit $url64 -Checksum $checksum32 -ChecksumType $checksumType -Checksum64 $checksum64 -ChecksumType64 $ChecksumType
+
+# create .gui file
+New-Item -Path $dir -Name ($exe + '.gui') -ItemType file
+
+CreateStartMenuShortcut

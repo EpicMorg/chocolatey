f10ec2aead219dd6a6a957fcab91b56aff93814d
STAM <stam@epicm.org>
2026-03-20 14:40:11 +0300

    winbox 4.0.1 release

---
+++ chocolatey/winbox/4.0.1/CHANGELOG.md
@@ -0,0 +1,32 @@
+## WinBox - [Changelog](https://forum.mikrotik.com/t/winbox-v4-0-1-released/268595)
+
+### What's new in v4.0.1:
+* table: fixed a visual issue in tables with tree structure where last element is collapsed;
+
+### What's new in WinBox 4:
+
+* WinBox is now cross-platform and available on Windows, Linux, and macOS;
+* completely redesigned the login panel, tables, forms, buttons, and overall UI layout;
+* added custom font selection and font weight/spacing configuration;
+* added dark mode support;
+* added fast window overview and switching Alt+S, Alt+A (similar to OS task switching);
+* added full screen support;
+* added global menu search support Alt+F;
+* added more UI zoom levels and support UI spacing adjustments;
+* added new terminal shortcut Alt+T;
+* added support for connecting to multiple devices directly from the login panel's neighbor table;
+* added table spacing, separator, comment column order configuration features;
+* added Workspaces for faster window management;
+* always use single window (no more OS popup clutter);
+* form: combo boxes now show extra information (flags, icons, comments, errors);
+* form: real-time field validation and error reporting;
+* form: support more field types;
+* form: support opening multiple tabs using Shift + left-click;
+* standardized text encoding to UTF-8 across the applications;
+* table: added tree structure collapse/expand feature;
+* table: more efficient File window;
+* table: resize column width to content on double click;
+* table: support customizing visible window tabs;
+* table: support freezing cell updates on all tables;
+* table: support more than 2 column sorting;
+* table: support opening multiple table windows;
---
+++ chocolatey/winbox/4.0.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+﻿$packageName = 'winbox'
+$appName = 'WinBox'
+$version = '4.0.1'
+$url = "https://download.mikrotik.com/routeros/winbox/$version/WinBox_Windows.zip"
+
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$is64 = (Get-ProcessorBits 64)
+If (!$is64) { throw "WinBox 4 requires a 64-bit operating system." }
+
+$processName = 'WinBox'
+$exe = 'WinBox.exe'
+$exe64 = 'winbox64.exe'
+$fullPath = Join-Path -Path $toolsDir -ChildPath $exe
+$fullPath64 = Join-Path -Path $toolsDir -ChildPath $exe64
+
+$checksum = '179D58F13CF2D580097620A091414477B742D142ED1DB20EA5E6129C3B2A561D' 
+$checksumType = 'sha256'
+
+$startFolder = $appName
+
+Function CreateStartMenuShortcut {
+    $startMenuFolderPath = Join-Path -Path ([Environment]::GetFolderPath('Programs')) -ChildPath $startFolder
+    New-Item $startMenuFolderPath -Type Directory -ErrorAction SilentlyContinue
+    If (Test-Path -Path $startMenuFolderPath -PathType Container) {
+            Install-ChocolateyShortcut -shortcutFilePath `
+                (Join-Path -Path $startMenuFolderPath -ChildPath ($appName + '.lnk')) `
+                -targetPath $fullPath -workingDirectory $toolsDir
+    } Else {
+        Write-Warning "Start menu folder couldn't be created."
+    }
+}
+
+Stop-Process -Name $processName -ErrorAction SilentlyContinue
+Stop-Process -Name "winbox64" -ErrorAction SilentlyContinue
+
+Install-ChocolateyZipPackage -PackageName $packageName `
+    -Url $url `
+    -UnzipLocation $toolsDir `
+    -Checksum $checksum `
+    -ChecksumType $checksumType
+
+Copy-Item -Path $fullPath -Destination $fullPath64 -Force
+
+New-Item -Path $toolsDir -Name ($exe + '.gui') -ItemType file -Force | Out-Null
+New-Item -Path $toolsDir -Name ($exe64 + '.gui') -ItemType file -Force | Out-Null
+
+CreateStartMenuShortcut
